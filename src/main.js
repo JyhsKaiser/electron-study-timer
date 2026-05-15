c08c0cb579d054c83b0d3e60d2e922704e79aad1
“Jovani <jyhskaiser@gmail.com>
2026-05-15 16:29:54 -0600

    style(ui): incrementar ancho del widget a 550px y optimizar legibilidad del reloj

--- src/main.js
+++ src/main.js
@@ -46,13 +46,14 @@ app.on('window-all-closed', () => {
 ipcMain.on('toggle-widget-mode', (event, isWidget) => {
     if (isWidget) {
         // Modo Widget: Pequeño y siempre visible
-        mainWindow.setSize(300, 300);
+        mainWindow.setSize(550, 300);
         mainWindow.setAlwaysOnTop(true, 'screen-saver'); // Se asegura de quedar por encima de todo
-        // Opcional: mainWindow.setOpacity(0.9); para darle un toque translúcido
+        Opcional: mainWindow.setOpacity(0.8);
     } else {
         // Modo Configuración: Tamaño normal y comportamiento estándar
         mainWindow.setSize(800, 600);
         mainWindow.setAlwaysOnTop(false);
+        Opcional: mainWindow.setOpacity(0.8);
     }
 
     // Centramos la ventana para que el cambio de tamaño no la deje en una esquina extraña
